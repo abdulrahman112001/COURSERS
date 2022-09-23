aed8234e633ea58279182bac26d40e0a3e81d24f
abdulrahman112001 <101294492+abdulrahman112001@users.noreply.github.com>
2022-09-24 00:42:46 +0200

    push
    push

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+
+var topBTN = document.getElementById("header-nav-bar")
+
+
+window.addEventListener("scroll",function(){
+    // console.log("window is scrolling");
+
+    if (document.documentElement.scrollTop > 10) {
+        topBTN.classList.add("header-top") ;
+        if(document.documentElement.scrollTop > 500){
+             topBTN.classList.add("header-bottom")
+        }else{
+            topBTN.classList.remove("header-bottom")
+
+        }
+    }
+    else  topBTN.classList.remove("header-top")
+})
+
+
+
+
+
+
+
 function addTag(e,input) {
     if(e.which == 13 && input.value) {
         $('.team-members').append('<span>'+ input.value +'</span>')
